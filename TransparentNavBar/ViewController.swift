964043892a10dc5991050367ecb5695557da4268
praveen <praveen.vundyala@gmail.com>
2019-03-30 22:55:03 +0530

    Initial Commit

---
+++ TransparentNavBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TransparentNavBar
+//
+//  Created by praveen reddy on 3/30/19.
+//  Copyright © 2019 praveen reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
